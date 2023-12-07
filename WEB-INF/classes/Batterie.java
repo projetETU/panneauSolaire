c09dcdb987150872043bcc29957918d060bc7de8
Mendrika Rs <men.malala@gmail.com>
2023-12-07 15:51:24 +0300

    Update Batterie.java

--- WEB-INF/classes/Batterie.java
+++ WEB-INF/classes/Batterie.java
@@ -1,6 +1,26 @@
 package solaire;
 public class Batterie {
-    double intensite;
-    double tension;
-    
+    int  intensite;
+    int  tension;
+    int  puissance;
+    public int getIntensite() {
+        return intensite;
+    }
+    public void setIntensite(int intensite) {
+        this.intensite = intensite;
+    }
+    public int getTension() {
+        return tension;
+    }
+    public void setTension(int tension) {
+        this.tension = tension;
+    }
+    public int getPuissance() {
+        return puissance;
+    }
+    public void setPuissance() {
+        // puissance a 50%
+        int puissance =  getIntensite()*getTension();
+        this.puissance = puissance*50/100 ;
+    }
 }
