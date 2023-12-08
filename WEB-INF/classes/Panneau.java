4d444720b8270d865c0735339af949ba180a1838
Mendrika Rs <men.malala@gmail.com>
2023-12-08 16:27:56 +0300

    2.4

--- WEB-INF/classes/Panneau.java
+++ WEB-INF/classes/Panneau.java
@@ -1,11 +1,20 @@
 package solaire;
+
+import meteo.Meteo;
+
 public class Panneau {
     int puissance;
     public int getPuissance(){
         return puissance;
     }
     public void setPuissance(int puissance){
+        puissance = 50000;
         this.puissance = puissance;
+    }
+    public double puissanceDelivrer(int luminositer)
+    {
+        Meteo meteo = new Meteo();
+        
     }
     
 }
